e17db58ae21c99000d0add8a4b581f257a39b61d
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-07-20 16:34:06 +0530

    commits

--- testing-server-v4/config/allowedOrigins.js
+++ testing-server-v4/config/allowedOrigins.js
@@ -3,5 +3,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3020'
 ];
+//add your domain to the list
+//this is a list of allowed origins for CORS
 
 module.exports = allowedOrigins;
