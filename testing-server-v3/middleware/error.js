c2859423b2f5a3da4c846e33e0d9b2a1fd25fd78
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-07-22 01:49:19 +0530

    Delete testing-server-v3 directory

--- testing-server-v3/middleware/error.js
+++
@@ -1,6 +0,0 @@
-const sendError = (req, res) => {
-    console.log("Error 404: Page not found")    
-    res.status(404).render('404')
-    return
-}
-module.exports = sendError;
--- testing-server-v3/routes/refresh.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const refreshTokenController = require('../controller/refreshTokenController');
-
-router.get('/', refreshTokenController.handleRefreshToken, (req, res) => {
-    res.render('auth')
-});
-
-module.exports = router;
