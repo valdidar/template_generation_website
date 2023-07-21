da7b6de10e8fcee0ad50f1f43fab8b94bf52d8a4
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-07-22 01:49:06 +0530

    Delete testing-server directory

--- testing-server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/authController');
-
-router.post('/', authController.handleLogin);
-
-module.exports = router;
--- testing-server/routes/templates.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const templateAction = require('../controller/templateAction');
-
-router.post('/', templateAction.templateAdition);
-
-module.exports = router;
