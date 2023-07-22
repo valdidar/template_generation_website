df0005d791d5cc6ed8e26f376b1592842a75c303
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-07-22 18:39:49 +0530

    Delete testing-server-v4 directory

--- testing-server-v4/routes/logout.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const logoutController = require('../controller/logoutController');
-router.get('/', logoutController.handleLogout, (req, res, next) => {
-    res.redirect('/auth');
-})
-
-module.exports = router;
