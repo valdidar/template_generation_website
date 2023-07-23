701f7c7a6c1ec21a7cfcf895ae023a12cb0049ad
val didar singh <95515558+valdidar@users.noreply.github.com>
2023-07-23 14:37:35 +0000

    added the node modules

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:5500',
-    'http://localhost:3020'
+    'http://localhost:3020',
+    'https://valdidar-expert-space-robot-wq7rv4vvpp72v9rr-3020.preview.app.github.dev'
 ];
 //add your domain to the list
 //this is a list of allowed origins for CORS
